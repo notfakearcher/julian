000e890e54ff16c969117b781d257559ad50d320
JulianArcher-PEP <Julian.Archer@pepsico.com>
2024-09-29 16:51:39 -0400

    2024_09_29_16_51

---
+++ 01_practice/javascript/alerts.js
@@ -0,0 +1,11 @@
+// Alert 1
+alert("Hello World");
+
+// Alert 2
+alert("Hello Bao Bao!")
+
+// Alert 3
+alert("We are now in business");
+
+// Alert 4
+alert("Example with line return.\nThis is in a new line.");
