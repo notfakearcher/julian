5a822845c58d5a0d7209bd19649ba8e55241176e
JulianArcher-PEP <Julian.Archer@pepsico.com>
2024-10-08 18:41:39 -0400

    2024_10_08_18_41

---
+++ 01_practice/javascript/math_expressions.js
@@ -0,0 +1,2 @@
+let total = 2 + 2
+alert(total)
--- 01_practice/javascript/variables.js
+++ 01_practice/javascript/variables.js
@@ -2,6 +2,27 @@
 // Variables -------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 
-let animal;
-animal = 'Turtle';
-alert(animal);
+// let animal;
+// animal = 'Turtle';
+// console.log(animal);
+// // alert(animal);
+
+// let weight = 150;
+// console.log(weight);
+
+// let originalNum = 23;
+// let numToBeAdded = 7;
+// let newNum = originalNum + numToBeAdded;
+// console.log(newNum);
+
+// let aNum = 90;
+// let aNum = aNum + 10; // doesnt work (already declared)
+// aNum = aNum + 10;
+// console.log(aNum);
+
+// let aNum = 23;
+// let newNum = aNum + 7
+// console.log(newNum);
+
+
+alert(144);
