49096771ed49ebf5879f0d899993d6a06cc576bd
JulianArcher-PEP <Julian.Archer@pepsico.com>
2025-06-05 22:05:15 -0400

    2025_06_05_22_05

--- 01_practice/sql/data_cleaning_and_transformation.sql
+++ 01_practice/sql/data_cleaning_and_transformation.sql
@@ -99,12 +99,91 @@ FROM customers;
 -- DATE/TIME FUNCTIONS
 -- -----------------------------------------------------------------------------
 
+-- Using NOW(), CURRENT_DATE
+SELECT *
+FROM orders;
 
+SELECT NOW() AS current_datetime;
+
+SELECT CURRENT_DATE AS today_date;
+
+
+SELECT
+  order_date,
+  NOW() AS today_datetime,
+  CURRENT_DATE AS today_date
+FROM orders;
+
+-- USING DATEDIFF
+SELECT
+  order_date,
+  NOW() AS today_datetime,
+  CURRENT_DATE AS today_date,
+  DATEDIFF(NOW(), order_date) AS elapsed_days
+FROM orders;
+
+
+-- Using EXTRACT (part FROM date), DAYOFYEAR, DAYOFMONTH, DAYOFWEEK
+SELECT
+  order_date,
+  EXTRACT(YEAR FROM order_date) AS order_year,
+  EXTRACT(MONTH FROM order_date) AS order_month,
+  EXTRACT(DAY FROM order_date) AS order_day,
+
+  EXTRACT(QUARTER FROM order_date) AS order_quarter,
+  EXTRACT(WEEK FROM order_date) AS order_week,
+
+  DAYOFWEEK(order_date) AS order_dayofweek,
+  DAYOFMONTH(order_date) AS order_dayofmonth,
+  DAYOFYEAR(order_date) AS order_dayofyear,
+
+  NOW() AS today_date,
+  EXTRACT(HOUR FROM NOW()) AS current_hour,
+  EXTRACT(MINUTE FROM NOW()) AS current_minute,
+  EXTRACT(SECOND FROM NOW()) AS current_second,
+  EXTRACT(MICROSECOND FROM NOW()) AS current_microsecond
+FROM orders;
+
+
+FROM orders;
 
 -- -----------------------------------------------------------------------------
 -- TYPE CASTING/ CONVERSION
 -- -----------------------------------------------------------------------------
 
+-- Using CAST(expr AS type): SIGNED/UNSIGNED, CHAR/CHARACTER, BINARY 
+-- Using CONVERT is similar; just don't use AS key word and use ',' instead
+SELECT *
+FROM orderitems;
+
+SELECT CAST('123' AS UNSIGNED) AS as_number;
+SELECT CAST('-123' AS SIGNED) AS as_number;
+
+SELECT CAST(45 AS CHAR) AS as_char;
+
+SELECT CAST(12 AS BINARY) AS as_binary;
+-- SELECT BIN(12) AS as_binary;
+
+
+-- Using CAST(expr AS type) :DECIMAL(M,D)
+SELECT
+  quantity,
+  CAST(quantity AS DECIMAL(5,2)) AS quantiy_decimal
+FROM orderitems;
+
+SELECT CAST('-123' AS DECIMAL(5,2)) AS as_deciaml;
+SELECT CONVERT('-123', DECIMAL(5,2)) AS as_deciaml;
+
+
+
+
+
+-- Using CAST(expr AS type): DATE, DATETIME, TIME
+SELECT CAST('2025-06-01' AS DATE) AS today_date;
+
+SELECT CAST('2025-06-01' AS DATETIME) AS today_datetime;
+
+SELECT CAST('2025-06-01 21:09:37' AS TIME) AS today_time;
 
 
 
@@ -130,3 +209,23 @@ REGEXP_REPLACE(
   ', '
 ) AS vend_email_striped
 FROM vendors;
+
+-- Using DATE_FORMAT
+SELECT
+  order_date,
+  DATE_FORMAT(order_date, '%Y-%m-%d') AS order_date2,
+  DATE_FORMAT(order_date, '%Y-%b-%d') AS order_date3,
+  DATE_FORMAT(order_date, '%Y-%M-%d') AS order_date4,
+
+  DATE_FORMAT(order_date, '%y-%b-%d') AS order_date4,
+  DATE_FORMAT(order_date, '%Y-%b-%e') AS order_date5,
+
+  DATE_FORMAT(order_date, '%y-%m-%d') AS order_date6,
+
+  DATE_FORMAT(order_date, '%Y-%m-%e') AS order_date7,
+  DATE_FORMAT(order_date, '%Y-%m-%D') AS order_date8,
+
+  DATE_FORMAT(order_date, '%a') AS order_dayofweek_txt,
+
+  DATE_FORMAT(order_date, '%Y-01-01') AS year_start,
+  DATE_FORMAT(order_date, '%Y-%m-01') AS month_start
